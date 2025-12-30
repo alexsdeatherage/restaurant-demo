a0c26205efd2b3444ad0803a68b7ecd2be70f6b3
alexsdeatherage <alexdeatherage4829@gmail.com>
2025-12-29 21:36:11 -0800

    Remove Public Google API Key

--- components/sections/info-bar.tsx
+++ components/sections/info-bar.tsx
@@ -92,7 +92,7 @@ export function InfoBar() {
         {/* Map Embed */}
         <div className="aspect-video w-full overflow-hidden rounded-lg">
           <iframe
-            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=${encodeURIComponent(siteConfig.contact.address)}`}
+            src={``}
             width="100%"
             height="100%"
             style={{ border: 0 }}
